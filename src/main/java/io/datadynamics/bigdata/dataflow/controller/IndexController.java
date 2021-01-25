6a27028e6e16f2496f4e6ed0838ebb6e0d992f10
Forrest <changeme1006@gmail.com>
2021-01-25 13:18:59 +0900

    template setting

--- src/main/java/io/datadynamics/bigdata/dataflow/controller/IndexController.java
+++ src/main/java/io/datadynamics/bigdata/dataflow/controller/IndexController.java
@@ -1,13 +1,11 @@
 package io.datadynamics.bigdata.dataflow.controller;
 
-import freemarker.core.OutputFormat;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,33 +13,33 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/service")
 public class IndexController {
-    @Autowired
-    FreeMarkerConfigurer configurer;
-    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
 
-    @GetMapping("/blogpost")
-    public ResponseEntity blogpost() throws IOException, TemplateException {
-        Template template = configurer.getConfiguration().getTemplate("blogpost.ftl");
-        Map<String, String> message = new HashMap<>();
-        message.put("message", "Hello, World");
-        template.process(message, new StringWriter());
-        OutputFormat outputFormat = template.getOutputFormat();
+    @RequestMapping("/blogpost")
+    public String blogpost(Model model) throws IOException, TemplateException {
+        Template template = configurer.getConfiguration().getTemplate("freemarker/blogpost.ftl");
+//        Map<String, String> message = new HashMap<>();
+        model.addAttribute("message", "Hello, 경민");
+        StringWriter sw = new StringWriter();
+        template.process(model, sw);
 
-        logger.info("template :: {}", outputFormat.toString());
-        return ResponseEntity.ok().build();
+        logger.info("template.getSourceName() :: {}", template.getSourceName());
+        logger.info("template.getName() :: {}", template.getName());
+        logger.info("template :: {}", sw.toString());
+        logger.info("model :: {}", model);
+        return "blogpost";
     }
 
     //
     @GetMapping("/index")
-    public ModelAndView index() throws IOException, TemplateException {
+    public ModelAndView index() {
         ModelAndView mav = new ModelAndView("index");
-        mav.addObject("name", "World");
+        mav.addObject("name", "경민");
         logger.info("mav :: {}", mav);
         return mav;
     }
